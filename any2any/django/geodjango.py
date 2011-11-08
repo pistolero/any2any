b5ceca1026430bd42dbe632e410528604a52809e
Sébastien Piquemal <sebpiq@gmail.com>
2011-11-08 23:22:15 +0200

    dirty fixes for geodjango

--- any2any/django/geodjango.py
+++ any2any/django/geodjango.py
@@ -7,6 +7,24 @@ GEODJANGO_FIELDS = (GeometryField, PointField, LineStringField,
     PolygonField, MultiPointField, MultiLineStringField, MultiPolygonField,
     GeometryCollectionField)
 
+import copy
+import datetime
+import types
+
+from django.db import models
+from django.http import QueryDict
+from django.db.models.fields.related import ManyRelatedObjectsDescriptor, ForeignRelatedObjectsDescriptor
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.generic import GenericForeignKey, GenericRelation
+from django.db.models.query import QuerySet
+from django.core.files.base import ContentFile
+from django.core.files import File
+from bundle import *
+from any2any import *
+from any2any.bundle import ValueInfo
+from any2any.utils import classproperty
+from any2any.stdlib.bundle import DateTimeBundle, DateBundle
+
 
 # GeoDjango
 #======================================
@@ -64,13 +82,13 @@ class MultiPolygonBundle(GEOSGeometryBundle):
     value_type = Polygon
 
 
-class ModelBundle(ObjectBundle):
+# ModelBundle
+#======================================
+class GeoModelMixin(ModelMixin):
 
     @classmethod
-    def _build_schema(cls, fields_dict):
-        schema = super(ModelBundle, cls)._build_schema(fields_dict)
-        for name, f in fields_dict.items():
-            ftype = type(f)
+    def _wrap(cls, f):
+        ftype = type(f)
         if isinstance(f, GEODJANGO_FIELDS):
             if isinstance(f, PointField):
                 geom_type = Point
@@ -84,10 +102,91 @@ class ModelBundle(ObjectBundle):
                 geom_type = MultiLineBundle
             elif isinstance(f, MultiPolygonField):
                 geom_type = MultiPolygonBundle
-            v = ValueInfo(geom_type, lookup_with=(ftype, geom_type))
+            return ValueInfo(geom_type, lookup_with=(ftype, geom_type))
+        else:
+            return super(GeoModelMixin, cls)._wrap(f)
 
 
 
+class ReadOnlyModelBundle(GeoModelMixin, ObjectBundle):
+    """
+    A subclass of :class:`daccasts.ObjectBundle` for django models. Only allows to retrieve objects that exist in the database, e.g. :
+
+        >>> obj = ReadOnlyModelBundle({'pk': 12, 'a_field': 'new_value'})
+        >>> obj.a_field != 'new_value'
+        True
+    """
+
+    def iter(self):
+        if not self.obj is None: # TODO: quick fix for the fact that FK can be null
+            return super(ReadOnlyModelBundle, self).iter()
+        else:
+            raise StopIteration
+
+    @classmethod
+    def factory(cls, items_iter):
+        items = dict(items_iter)
+        try:
+            return cls.retrieve(**items)
+        except cls.model.DoesNotExist as e:
+            raise cls.model.DoesNotExist('The ModelBundle is read only and %s' % e)
+        except cls.model.MultipleObjectsReturned:
+            raise ValueError("'%s' is not unique for '%s'" %
+            (cls.key_schema, cls.model))
+
+
+class UpdateOnlyModelBundle(GeoModelMixin, ObjectBundle):
+    """
+    A subclass of :class:`daccasts.ObjectBundle` for django models. Only allows to retrieve and update objects that exist in the database, e.g. :
+
+        >>> obj = UpdateOnlyModelBundle({'pk': 12, 'a_field': 'new_value'})
+        >>> obj.a_field == 'new_value'
+        True
+    """
+
+    def iter(self):
+        if not self.obj is None: # TODO: quick fix for the fact that FK can be null
+            return super(UpdateOnlyModelBundle, self).iter()
+        else:
+            raise StopIteration
+
+    @classmethod
+    def factory(cls, items_iter):
+        items = dict(items_iter)
+        try:
+            bundle = cls.retrieve(**items)
+        except cls.model.DoesNotExist as e:
+            raise cls.model.DoesNotExist('The ModelBundle is update only and %s' % e)
+        except cls.model.MultipleObjectsReturned:
+            raise ValueError("'%s' is not unique for '%s'" %
+            (cls.key_schema, cls.model))
+        return bundle.update(**items)
+
+
+class CRUModelBundle(GeoModelMixin, ObjectBundle):
+    """
+    A subclass of :class:`daccasts.ObjectBundle` for django models. Allows to retrieve/create and update objects.
+    """
+
+    def iter(self):
+        if not self.obj is None: # TODO: quick fix for the fact that FK can be null
+            return super(CRUModelBundle, self).iter()
+        else:
+            raise StopIteration
+
+    @classmethod
+    def factory(cls, items_iter):
+        items = dict(items_iter)
+        try:
+            bundle = cls.retrieve(**items)
+        except cls.model.MultipleObjectsReturned:
+            raise ValueError("'%s' is not unique for '%s'" %
+            (cls.key_schema, cls.model))
+        except cls.model.DoesNotExist:
+            bundle = cls.create(**items)
+        return bundle.update(**items)
+
+import datetime
 serialize = Cast({
     AllSubSetsOf(dict): MappingBundle,
     AllSubSetsOf(list): IterableBundle,
